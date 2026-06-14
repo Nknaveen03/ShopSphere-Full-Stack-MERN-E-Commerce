f1174ff3e0b2dae959480e1024c538c9b3d747d4
Nknaveen03 <naveennk6384@gmail.com>
2026-06-14 21:15:25 +0530

    hardcode API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,7 +3,7 @@
 // ============================================================
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const API_URL = 'https://shopsphere-full-stack-mern-e-commerce.onrender.com/api';
 
 // Create axios instance with base URL
 const api = axios.create({
